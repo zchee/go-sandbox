bfc33960770c82f57cbf2d37e1fa234558fe6650
Koichi Shiraishi <zchee.io@gmail.com>
2022-04-25 06:21:42 +0900

    utf8dump: support variadic args
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- utf8dump/utf8dump.go
+++ utf8dump/utf8dump.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("Usage: utf8dump <word>")
+		log.Fatal("Usage: utf8dump word [word...]")
 	}
 
-	w := os.Args[1]
-	fmt.Printf("%+q\n", w)
+	for _, s := range os.Args[1:] {
+		if idx := strings.Index(s, "U"); idx > -1 {
+			s = s[idx+2:]
+			u, _ := strconv.ParseUint(s, 16, 32)
+			r := rune(u)
+			fmt.Printf("%2s\n%2s\n", s, string(r))
+		}
+		fmt.Printf("%2s\n%+2q\n", s, s)
+	}
 }
